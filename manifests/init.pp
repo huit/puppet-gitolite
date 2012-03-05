f28eb37ce8d749384dfadf83b18586348dbbe6b6
Steve Huff <shuff@vecna.org>
2012-03-05 09:49:07 -0500

    initializing huit-gitolite module

---
+++ manifests/init.pp
@@ -0,0 +1,17 @@
+# Class: gitolite
+#
+# This module manages gitolite
+#
+# Parameters:
+#
+# Actions:
+#
+# Requires:
+#
+# Sample Usage:
+#
+# [Remember: No empty lines between comments and class definition]
+class gitolite {
+
+
+}
