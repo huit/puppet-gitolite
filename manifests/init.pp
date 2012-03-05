1adc3268b5a2888a1d8b16f97b711d16d9a44004
Steve Huff <shuff@vecna.org>
2012-03-05 14:17:42 -0500

    included GPL and copyright notice

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,6 +1,6 @@
 # Class: gitolite
 #
-# This module manages gitolite
+# This module installs gitolite and performs initial configuration.
 #
 # Parameters:
 #
@@ -134,3 +134,18 @@ class gitolite (
       refreshonly => true;
   }
 }
+#
+# Copyright (C) 2012 Steve Huff <steve_huff@harvard.edu>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
